d3b20e0214a8bad5584862e4a89e978699d39dfc
smaulik557-admin <smaulik557@gmail.com>
2025-10-29 21:31:38 +0530

    Added initial PayPal order controller and service implementation with dummy createOrder method

---
+++ src/main/java/com/hulkhiretech/payments/controller/PaymentController.java
@@ -0,0 +1,34 @@
+package com.hulkhiretech.payments.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hulkhiretech.payments.service.interfaces.PaymentService;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentController {
+
+	private final PaymentService paymentService; 
+
+	@PostMapping("/payments/order-create")
+	public String createOrder()	{
+
+		log.info("Creating order in PayPal order service");
+		String response = paymentService.createOrder();
+		log.info("Order creation response from service response: {}", response);
+
+		return response;
+	}
+
+	@PostConstruct
+	public void init()	{
+		log.info("Initializing PaymentController" + "paymentService: {}", paymentService);
+	}
+
+}
---
+++ src/main/java/com/hulkhiretech/payments/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hulkhiretech.payments.service.impl;
+
+
+import org.springframework.stereotype.Service;
+import com.hulkhiretech.payments.service.interfaces.PaymentService;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+	@Override
+	public String createOrder() {
+		
+		log.info("Creating order in PaymentServiceImpl");
+		
+		/* TODO
+		 * 1. getAccessToken (OAuth) - DONE
+		 * 2. Call PayPal createOrder
+		 * 3. Success/Failure/TimeOut - Proper response handling
+		 * 4. What to return to your calling service (payment-processing-service)
+		 */
+
+		return "Order Created from service";
+	}
+
+	@PostConstruct
+	public void init()	{
+		log.info("Initializing PaymentServiecImpl");
+	}
+
+}
